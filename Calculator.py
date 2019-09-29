a376a894e97b61c46d12308bdf7bbfff263127b0
jacobrichard1 <54617665+jacobrichard1@users.noreply.github.com>
2019-09-29 22:52:46 +0400

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,16 @@
+x = int(input("Choose a number"))
+
+y = int(input("Choose another number"))
+
+operation = input("Choose an operator: +,-,/,*")
+
+if operation == ("+"):
+    print(x + y)
+if operation == ("-"):
+    print(x - y)
+if operation == ("/"):
+    print(x / y)
+if operation == ("*"):
+    print(x * y)
+  
+  
